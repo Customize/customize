5f1e2ef9e252fbcb6790c8e2c221d53024273eae
Londerson <londerson@gmail.com>
2014-10-30 20:56:15 -0200

    listing posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,10 @@
 class Admin::PostsController < Admin::ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
-
 end
--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Admin::PostsController, type: :controller do
+  
+  describe "GET index" do
+    before :each do
+      @post  = Post.create(title: "Post Title")
+      @post2 = Post.create(title: "Post Title2")
+    end
+
+    it "return all posts" do
+      get :index
+      expect(assigns(:posts).to_a).to eq([@post, @post2])
+    end
+  end
+
   describe "GET show" do
     before :each do
       @post = Post.create(title: "Title Post")     
