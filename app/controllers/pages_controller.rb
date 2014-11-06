52b65a6ff8eed13c1d7060e846e32b17a5017812
Londerson <londerson@gmail.com>
2014-11-05 23:09:08 -0200

    get posts of page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find(params[:id])
+
+    if @page.posts.size >= 1
+      @posts = @page.posts
+    end
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 describe PagesController, type: :controller do
-
   before :each do
     @page = Page.create(title: "Page Title")
   end
@@ -10,6 +9,20 @@ describe PagesController, type: :controller do
     it "return current page" do
       get :show, id: @page
       expect(assigns(:page).title).to eq("Page Title")
+    end
+
+    context "when current page have posts" do
+      before :each do
+        @postA = Post.create(title: "Title A")
+        @postB = Post.create(title: "Title B")
+        @page.posts << [@postA, @postB]
+        @page.save!
+      end
+
+      it "show posts" do
+        get :show, id: @page
+        expect(assigns(:posts)).to eq([@postA, @postB])
+      end
     end
   end
 end
