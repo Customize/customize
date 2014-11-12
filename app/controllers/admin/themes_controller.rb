d7099524de3bf5b2479e3c33c7d9dd9228504fae
Londerson <londerson@gmail.com>
2014-11-11 23:24:25 -0200

    save themes and list

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -10,18 +10,22 @@ class Admin::ThemesController < Admin::ApplicationController
   end
 
   def create
-      file = FileUploader.new
-      file.cache!(params[:theme][:file])
+    file = FileUploader.new
+    file.cache!(params[:theme][:file])
 
-      file = file.current_path
-      destination = "./lib/themes/"
+    file = file.current_path
+    destination = "./lib/themes/"
 
-      Zip::File.open(file) { |zip_file|
-       zip_file.each { |f|
-         f_path=File.join(destination, f.name)
-         FileUtils.mkdir_p(File.dirname(f_path))
-         zip_file.extract(f, f_path) unless File.exist?(f_path)
-       }
-      }
+    Zip::File.open(file) { |zip_file|
+     zip_file.each { |f|
+       f_path=File.join(destination, f.name)
+       FileUtils.mkdir_p(File.dirname(f_path))
+       zip_file.extract(f, f_path) unless File.exist?(f_path)
+     }
+    }
+
+    Theme.save_themes
+
+    redirect_to admin_themes_path
   end
 end
