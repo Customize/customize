8cc560c33fef6e5ba93d01ec1e3ba2eb855da3a7
Londerson <londerson@gmail.com>
2014-11-22 10:32:07 -0200

    erase tests

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,23 +1,12 @@
 require "rails_helper"
 
-describe "save_themes" do
-  before :each do
-    @info = [{:name=>"Horizon", :description=>"lorem ipsum dolor sit", :author=>"Londerson", :site=>"londerson.github.io", :email=>"londerson@gmail.com", :version=>0.1}, {:name=>"Minimalist", :description=>"lorem ipsum dolor sit", :author=>"Londerson", :site=>"londerson.github.io", :email=>"londerson@gmail.com", :version=>0.1}]
-  end
-
-  it "save themes" do
-    Theme.save_themes(@info)
-    expect(Theme.count).to eq(2)
-  end
-
-  context "when have themes saved" do
-    before :each do
-      Theme.save_themes(@info)
-      Theme.save_themes(@info)
+describe Theme, type: :model do
+  context "without saved themes" do
+    xit "save theme" do
     end
-
-    it "not save repeated themes" do
-      expect(Theme.count).to eq(2)
+  end
+  context "when have themes saved" do
+    xit "save theme" do
     end
   end
 end
